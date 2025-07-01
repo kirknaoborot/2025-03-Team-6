a54794dbf071bfc5f2eb7d7883d641d3934d8a01
ilnur <ilnur.ibragimov.1993@yandex.ru>
2025-07-01 16:58:37 +0300

    add test consumer

---
+++ Team-6/TestConsumer/EventConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using MessageHubService.Domain.Entities;
+
+namespace TestConsumer
+{
+    class EventConsumer : IConsumer<ClientMessage>
+    {
+        public async Task Consume(ConsumeContext<ClientMessage> context)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Console.WriteLine($"message id {context.Message.Id}, message text {context.Message.MessageText}");
+        }
+    }
+}
---
+++ Team-6/TestConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using MassTransit;
+using MessageHubService.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TestConsumer;
+
+var host = CreateHostBuilder(args).Build();
+host.Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+     Host.CreateDefaultBuilder(args)
+      .ConfigureServices((hostContext, services) =>
+      {
+          var rabbitConfig = hostContext.Configuration
+          .GetSection(nameof(RabbitMqOptions))
+          .Get<RabbitMqOptions>() ?? throw new ArgumentException("Missing RabbitMq configuration section");
+
+          var recEndp = hostContext.Configuration
+           .GetSection(nameof(ReceiveEndpointOptions))
+           .Get<ReceiveEndpointOptions>() ?? throw new ArgumentException("Missing ReceiveEndpointOptions configuration section");
+
+          services.AddMassTransit(x =>
+          {
+              x.SetSnakeCaseEndpointNameFormatter();
+              x.AddConsumer<EventConsumer>();
+              x.UsingRabbitMq((context, cfg) =>
+              {
+                  cfg.Host(rabbitConfig.Host, "/", h => {
+                      h.Username(rabbitConfig.Username);
+                      h.Password(rabbitConfig.Password);
+                  });
+                  cfg.Message<ClientMessage>(m => m.SetEntityName(recEndp.ExchangeName));
+
+                  cfg.ReceiveEndpoint(recEndp.QueueName, e =>
+                  {
+                      e.ConfigureConsumeTopology = false;
+                      e.ConfigureConsumer<EventConsumer>(context);
+                      e.Bind<ClientMessage>(p =>
+                      {
+                          p.ExchangeType = "topic";
+                          p.RoutingKey = "high";
+                      });
+                      //e.UseMessageRetry(r =>
+                      //{
+                      // r.Incremental(3, TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(1));
+                      //});
+                  });
+              });
+          });
+      });
