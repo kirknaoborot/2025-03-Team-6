06f6bc1faefcebcbdc194a12c7a724c88f5a7455
ibragimov <ibragimov@satel.org>
2025-10-08 14:25:10 +0300

    a few refact TelegramBotService 2

--- Team-6/MessageHubService.Application/Services/TelegramBotService.cs
+++ Team-6/MessageHubService.Application/Services/TelegramBotService.cs
@@ -6,7 +6,7 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
-namespace MessageHubService.Application.Services.TelegramBot
+namespace MessageHubService.Application.Services
 {
     public class TelegramBotService : IBotWork, IMessageEvent
     {
